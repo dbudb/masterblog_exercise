45dc0153cea5eeb0ccca43d24442bc7f250989fd
Dennis Budek <dennis.budek@protonmail.com>
2026-07-24 11:42:21 +0200

    delete route

--- main.py
+++ main.py
@@ -52,5 +52,19 @@ def add():
     return render_template("add.html")
 
 
+@app.route("/delete/<int:post_id>")
+def delete(post_id):
+    """Delete a blog post and return to the homepage."""
+    posts = load_posts()
+
+    posts = [
+        post for post in posts
+        if post["id"] != post_id
+    ]
+
+    save_posts(posts)
+    return redirect(url_for("index"))
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
